5e677badab27bfb9a19ddf90781ba38201649a7b
Esther Guilland <esther.guilland@gmail.com>
2023-03-07 12:29:18 +0100

    created devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # NOTE! once the path exists change to >> root to: "searches#index"
 
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end
