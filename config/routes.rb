dc890f78858d85348a9306bedd58640136efb2fa
Linda Cesana <lincesana@gmail.com>
2023-03-07 12:54:01 +0100

    bookmarks model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :searches do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
