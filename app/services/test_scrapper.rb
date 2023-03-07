66215ac6be895a201251069bf5a1ccad48ff7029
Bruno Leote <b.leote@gmail.com>
2023-03-07 14:30:46 +0100

    Username added to user model, and services folder created

---
+++ app/services/test_scrapper.rb
@@ -0,0 +1,11 @@
+require "open-uri"
+require "mechanize"
+
+mechanize = Mechanize.new
+page = mechanize.get('https://www.merriam-webster.com/word-of-the-day')
+input = page.forms[0]
+input['s'] = 'awesome'
+new_page = input.submit
+  word_searched = new_page.search('h1.hword')
+puts new_page.uri
+puts "You successfully searched for the word #{word_searched.text.strip.upcase}."
