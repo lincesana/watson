4ab56e20732667927c17bb2a270d96f509a59aa1
Bruno Leote <b.leote@gmail.com>
2023-03-08 14:22:44 +0100

    https and scamdoc methods created in the services

--- app/services/scamdoc_service.rb
+++ app/services/scamdoc_service.rb
@@ -3,8 +3,10 @@ class ScamdocService
   require "open-uri"
   require "mechanize"
 
-  def initialize(company)
-    @company = company
+  def initialize(search_input)
+    @search_input = search_input
+    @https = nil
+    @scamdoc_rating = nil
   end
 
   def scamdoc_score
@@ -12,12 +14,22 @@ class ScamdocService
     page = mechanize.get('https://www.scamdoc.com/')
     input = page.forms[0]
     # pp input
-    input['expression'] = @company.to_s
+    input['expression'] = @search_input.to_s
     new_page = input.submit
     # puts new_page.uri
-    value = new_page.search("#final_score").attribute('value').value
-    https = new_page.search(".interrogation-list").inner_html.match?(/HTTPS/)
-    puts "#{input['expression']} score in ScamDoc is: #{value}."
-    puts "HTTPS presence in this website: #{https}."
+    value = new_page.search("#final_score").attribute('value').value.to_i
+    @scamdoc_rating = value
+  end
+
+  def https_presence
+    mechanize = Mechanize.new
+    page = mechanize.get('https://www.scamdoc.com/')
+    input = page.forms[0]
+    # pp input
+    input['expression'] = @search_input.to_s
+    new_page = input.submit
+    # puts new_page.uri
+    https_check = new_page.search(".interrogation-list").inner_html.match?(/HTTPS/)
+    @https = https_check
   end
 end
