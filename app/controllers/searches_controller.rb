001883a5b65efe6b4a8c23425bcc9a58511f6807
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-03-08 19:35:28 +0300

    searches controller and show page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,20 @@ class SearchesController < ApplicationController
     @searches = Search.where("rating>5")
   end
 
+  def create
+    @search = Search.new
+    @search.website_url = params["query"]
+    @search.trustpilot_verification = TrustpilotService.new(params["query"]).trustpilot.present?
+    @search.scandoc = ScamdocService.new(params["query"]).scamdoc_score
+    @search.https = ScamdocService.new(params["query"]).https_presence
+
+    if @search.save
+      redirect_to search_path(@search)
+    else
+      render 'index'
+    end
+  end
+
   def show
     @search = Search.find(params[:id])
   end
