906dd8672da80faed1a1d48dcabab7528f2a600c
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-03-10 15:01:42 +0300

    Instagram scrapper removed

--- app/services/instagram_scrapper.rb
+++
@@ -1,8 +0,0 @@
-require "mechanize"
-
-mechanize = Mechanize.new
-page = mechanize.get('https://www.castill.sk/')
-
-next_page = page.links_with(:href => /https:\/\/www.instagram.com\//).first.href
-
-pp next_page
